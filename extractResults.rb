cc8de351d715c1c1d4659936528fa475493ea82b
Panagiotis Papadopoulos <panpap@gmx.com>
2018-07-27 22:13:36 +0300

    source

---
+++ extractResults.rb
@@ -0,0 +1,43 @@
+require 'descriptive_statistics'
+
+def makeCDFs(col,total,filename)
+    parts=filename.gsub(".csv","")
+    system("awk -F\"\t\" 'NR==1{next} {print $"+col.to_s+"}' "+filename+" | sort -g | uniq -c | awk '{print $2\"\t\"$1\"\t\"$1*100/"+total.to_s+"}' | awk 'NR==1{print \"numOfSyncs\toccurences\tperc\tcdf\"} {s=s+$3;print $1\"\t\"$2\"\t\"$3\"\t\"s}' > "+parts+"_cdf.csv")
+end
+
+
+def mean(arr)
+	return  arr.inject{ |sum, el| sum + el }.to_f / arr.size
+end
+
+easy=Array.new
+privacy=Array.new
+regional=Array.new
+f1=File.new("easyListResults_"+ARGV[0],'w')
+f2=File.new("easyPrivacyResults_"+ARGV[0],'w')
+f3=File.new("easyChinaResults_"+ARGV[0],'w')
+File.foreach(ARGV[0]){|line|
+	if line.include? "blocked"
+		if line.include? "EasyList"
+			easy.push(line.split("blocked:").last.to_i)
+			f1.puts line.split("blocked:").last.to_i
+		end
+		if line.include? "EasyPrivacy"
+			privacy.push(line.split("blocked:").last.to_i) 
+			f2.puts line.split("blocked:").last.to_i
+		end
+		if line.include? "China"
+			regional.push(line.split("blocked:").last.to_i)
+			f3.puts line.split("blocked:").last.to_i
+		end
+	end
+}
+puts "Median EasyList: "+easy.percentile(50).to_s
+puts "Median EasyPrivacy: "+privacy.percentile(50).to_s
+puts "Median EasyChina:"+regional.percentile(50).to_s
+puts "Mean EasyList: "+mean(easy).to_s
+puts "Mean EasyPrivacy: "+mean(privacy).to_s
+puts "Mean EasyChina: "+mean(regional).to_s
+f1.close
+f2.close
+f3.close
---
+++ parser.js
@@ -0,0 +1,62 @@
+const puppeteer = require('puppeteer');
+const {AdBlockClient, FilterOptions} = require('ad-block')
+
+function loadLists(blockList) {
+    var client = new AdBlockClient();
+    console.log("reading "+blockList);
+    var fs = require('fs');
+    var content = fs.readFileSync(blockList, 'utf8');
+    var lines=content.split("\n")
+    for (var i=0;i<lines.length; i++){
+        if (lines[i].charAt(0) == "!") { //do nothing
+        } else {
+            client.parse(lines[i]) //"'"+line.split("\n")[0]+"'")
+        }
+    }
+    console.log("DONE with reading filter")
+    return client
+}
+
+function matchTrace(urls,name,client,list){
+    var i=0;
+    for (var j=0;j<urls.length;j++){
+        var b1 = client.matches(urls[j], FilterOptions.script, name)
+        if (b1) {
+            i = i + 1
+        }
+    }
+    //filtered results for the domain
+    console.log("List: "+list+" Domain "+name+" TotalReqs: "+urls.length+" blocked: "+i)//+" "+client.getMatchingStats().numExceptionHashSetSaves)
+}
+
+const easyListclient = loadLists("./blocklists/easylist.txt");
+const easyPrivacyclient= loadLists("./blocklists/easyprivacy.txt");
+const easyChinaclient=loadLists("./blocklists/easylistchina.txt");
+puppeteer.launch().then(async browser => {
+   // const browser = await puppeteer.launch();
+    console.log("puppeteer launched")
+    var fs = require('fs');
+    var content = fs.readFileSync("alexa10.txt",'utf8');//"alexa-top-1k.txt", 'utf8');
+    domains=content.split("\n")
+    for (var i=0;i<domains.length;i++){
+        var urls = [];
+        var domain = domains[i]
+        //if (domain.length>2) {
+            const page = await browser.newPage();
+            await page.setRequestInterception(true);
+            console.log("Running " + domain)
+            sitename = domain.split("//")[1]
+            page.on('request', interceptedRequest => {
+                //intercept requests and capture urls
+                urls.push(interceptedRequest.url())
+                interceptedRequest.continue();
+            });
+            await page.goto(domain);
+            // DONE with parsing
+        matchTrace(urls, sitename, easyListclient, "EasyList")
+        matchTrace(urls, sitename, easyPrivacyclient, "EasyPrivacy")
+            matchTrace(urls, sitename, easyChinaclient, "EasyChina")
+       // }
+    }
+    await browser.close();
+});
